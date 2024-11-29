dd5e437c4b6c3dfcfa3dbfd072fb8b9a587f8c6c
yunjie0572 <160023001+yunjie0572@users.noreply.github.com>
2024-11-30 00:20:16 +0800

    Add files via upload

---
+++ miner-coin.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+	./xmrig -a randomx --url randomx.rplant.xyz:17100 --tls --user ZEPHsAaWbb3ew67sQMApZUgK2ZCuajfLqLSU5z2PWQz81ojkKapc9Y6Rngn6HLqbKyHUWHVKZSEHAQXzB1TZzo6oAsD1yZT5tpD.vivo
+	sleep 5
+done
